49e65943e86375c55ef1549150402da4d2cbe71e
Forhad An Naim <developermithun1990@gmail.com>
2022-05-22 16:23:10 +0600

    Values printed

--- app/src/main/java/com/naim/androiddataclassnoargexample/MainActivity.kt
+++ app/src/main/java/com/naim/androiddataclassnoargexample/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.naim.androiddataclassnoargexample
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.naim.androiddataclassnoargexample.model.ThirdModel
 import com.naim.androiddataclassnoargexample.viewmodel.MainViewModel
 
+
 class MainActivity : AppCompatActivity() {
     val mainViewModel: MainViewModel by viewModels()
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val thirdModel = ThirdModel::class.java.newInstance()
-        println("Values ${thirdModel.id} ${thirdModel.title}")
+        println("Values ${mainViewModel.firstModel.toString()}")
+        println("Values ${mainViewModel.secondModel.toString()}")
+        println("Values ${mainViewModel.thirdModel.toString()}")
     }
 }
--- app/src/main/java/com/naim/androiddataclassnoargexample/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/naim/androiddataclassnoargexample/viewmodel/MainViewModel.kt
@@ -6,21 +6,21 @@ import androidx.lifecycle.ViewModel
 import com.naim.androiddataclassnoargexample.model.FirstModel
 import com.naim.androiddataclassnoargexample.model.SecondModel
 import com.naim.androiddataclassnoargexample.model.ThirdModel
-import kotlin.reflect.KClass
 import kotlin.reflect.full.createInstance
 
 class MainViewModel : ViewModel() {
-    var _firstModel: MutableLiveData<FirstModel> =
+    private var _firstModel: MutableLiveData<FirstModel> =
         MutableLiveData<FirstModel>(FirstModel(id = 1, title = "Title", subtitle = "Sub Title"))
     val firstModel: LiveData<FirstModel>
         get() = _firstModel
 
-    var _secondModel: MutableLiveData<SecondModel> =
+    private var _secondModel: MutableLiveData<SecondModel> =
         MutableLiveData<SecondModel>(SecondModel())
     val secondModel: LiveData<SecondModel>
         get() = _secondModel
 
-    var _thirdModel: MutableLiveData<ThirdModel> = MutableLiveData(ThirdModel::class.createInstance())
+    private var _thirdModel: MutableLiveData<ThirdModel> =
+        MutableLiveData(ThirdModel::class.createInstance())
     val thirdModel: LiveData<SecondModel>
         get() = thirdModel
 }
